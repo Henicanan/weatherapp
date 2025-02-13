dc8a95e2e6ff2473aa1128630be737a4132db33b
Ilya Kochurov <ilya.kochurov@nutnet.ru>
2025-02-13 19:06:51 +0400

    78049 feat: base styles added (fonts, mixins, variables etc.) #2

--- app.vue
+++ app.vue
@@ -1,3 +1,8 @@
-<template></template>
+<template>
+    <div class="welcome">
+        <h1>Welcome guys!</h1>
+    <p>Styles are working</p>
+  </div>
+</template>
 
 <style lang="scss" scoped></style>
