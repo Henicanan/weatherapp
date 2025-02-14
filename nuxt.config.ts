d3a79460450c7322945b484dc9b0be2f22c81fe6
MsIA-web <lodeynovm@gmail.com>
2025-02-14 10:50:48 +0400

    78050 feat: integrate OpenWeatherMap, configure env, and wrap axios

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,19 @@
 export default defineNuxtConfig({
-  compatibilityDate: "2025-02-13",
-  devtools: { enabled: true },
-  modules: ["@pinia/nuxt", "@vueuse/nuxt"],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: `@use "@/styles/global.scss" as *;`,
-        },
-      },
-    },
-  },
-});
+ compatibilityDate: '2025-02-13',
+ devtools: { enabled: true },
+ modules: ['@pinia/nuxt', '@vueuse/nuxt'],
+ runtimeConfig: {
+  private: {
+   openWeatherApiKey: process.env.NUXT_OPEN_WEATHER_API_KEY
+  }
+ },
+ vite: {
+  css: {
+   preprocessorOptions: {
+    scss: {
+     additionalData: `@use "@/styles/global.scss" as *;`
+    }
+   }
+  }
+ }
+})
