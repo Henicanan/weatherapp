e60a4903450e3eb44d62f5731461a5d792a18a62
Henicanan <116261477+Henicanan@users.noreply.github.com>
2025-02-23 18:33:39 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
       link: [{ rel: "icon", type: "image/svg+xml", href: "/icons/logo.svg" }],
       title: 'WeatherCheck'
     },
-    baseUrl: '/weatherapp/'
+    baseUrl: '/weatherapp'
   },
   vite: {
     css: {
